9c4efeb3c12ff0f0152a52e65986aac16a48589a
JimenaVega <jimevega28@gmail.com>
2023-01-13 16:41:53 -0300

    Intrucciones a traducir

---
+++ translator/translator.py
@@ -0,0 +1,7 @@
+# INTRUCTIONS:
+# R-TYPE
+# sll, srl, sra, sllv, srlv, srav, addu, subu, and, or, xor, nor, slt
+# I-TYPE
+# lb, lh, lw, lwu, lbu, lhu, sb, sh, sw, addi, andi, ori, xori, lui, slti, bew, bne, j, jal
+# J-TYPE
+# jr, jalr
