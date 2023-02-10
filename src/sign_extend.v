0d8b0226dc50956c39fd04ba6bd64592d0828535
AlejoSev <alejoseverini@gmail.com>
2023-02-10 17:53:07 -0300

    Cambios en sign_extend

--- src/sign_extend.v
+++ src/sign_extend.v
@@ -5,12 +5,12 @@ module sign_extend#(
         parameter NB_OUT = 32
     )
     (
-        input  [NB_IN-1:0]  i_data,
-        output [NB_OUT-1:0] o_data
+        input [NB_IN-1:0]  i_data,
+        output reg [NB_OUT-1:0] o_data
     );
 
-    always@(i_data) begin
+    always@(*) begin
         o_data[NB_IN-1:0] = i_data[NB_IN-1:0];
-        o_data[NB_OUT-1:0] = {NB_IN-1{i_data[NB_IN-1]}}; // se extiende el signo de i_data[15]
+        o_data[NB_OUT-1:NB_IN] = {NB_IN{i_data[NB_IN-1]}}; // se extiende el signo de i_data[15]
     end    
 endmodule
