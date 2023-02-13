70d5787162b32c815a997213cb3da3eea8b458ed
JimenaVega <jimevega28@gmail.com>
2023-02-12 21:49:26 -0300

    Fixeando conflicto de merge

--- src/sign_extend.v
+++ src/sign_extend.v
@@ -9,10 +9,8 @@ module sign_extend#(
         output reg [NB_OUT-1:0] o_data
     );
 
-    // assign o_data = i_data;
-    always@(i_data) begin
+    always@(*) begin
         o_data[NB_IN-1:0] = i_data[NB_IN-1:0];
-        o_data[NB_OUT-1:0] = {NB_IN-1{i_data[NB_IN-1]}}; // se extiende el signo de i_data[15]
-    end
-    
+        o_data[NB_OUT-1:NB_IN] = {NB_IN{i_data[NB_IN-1]}}; // se extiende el signo de i_data[15]
+    end    
 endmodule
