3bde68bf9eb116c684dd2bf761b210819dce7892
AlejoSev <alejoseverini@gmail.com>
2023-03-06 17:09:09 -0300

    Cambios en data memory

---
+++ sim/tb_data_memory.v
@@ -0,0 +1,57 @@
+module tb_data_memory;
+
+  // Parameters
+  localparam  MEMORY_WIDTH = 8;
+  localparam  MEMORY_DEPTH = 128;
+  localparam  NB_ADDR = 7;
+  localparam  NB_DATA = 32;
+  localparam  RAM_PERFORMANCE = "LOW_LATENCY";
+  localparam  INIT_FILE = "";
+
+  // Ports
+  reg i_clock = 0;
+  reg i_mem_write_flag = 0;
+  reg i_mem_read_flag = 0;
+  reg i_word_en = 0;
+  reg i_halfword_en = 0;
+  reg i_byte_en = 0;
+  reg [NB_ADDR-1:0] i_address;
+  reg [NB_DATA-1:0] i_write_data;
+  wire [NB_DATA-1:0] o_read_data;
+
+  data_memory 
+  #(
+    .MEMORY_WIDTH(MEMORY_WIDTH ),
+    .MEMORY_DEPTH(MEMORY_DEPTH ),
+    .NB_ADDR(NB_ADDR ),
+    .NB_DATA(NB_DATA ),
+    .RAM_PERFORMANCE(RAM_PERFORMANCE ),
+    .INIT_FILE (
+        INIT_FILE )
+  )
+  data_memory_1 (
+    .i_clock (i_clock ),
+    .i_mem_write_flag (i_mem_write_flag ),
+    .i_mem_read_flag (i_mem_read_flag ),
+    .i_word_en (i_word_en ),
+    .i_halfword_en (i_halfword_en ),
+    .i_byte_en (i_byte_en ),
+    .i_address (i_address ),
+    .i_write_data (i_write_data ),
+    .o_read_data  ( o_read_data)
+  );
+
+  initial begin
+    begin
+      i_mem_write_flag  = 1'b1;
+      i_word_en         = 1'b1;
+      i_address         = 7'b0;
+      i_write_data      = 32'hffffffff;
+      $finish;
+    end
+  end
+
+  always
+    #5  i_clock = ! i_clock ;
+
+endmodule
--- src/data_memory.v
+++ src/data_memory.v
@@ -6,32 +6,44 @@
 //  If a reset or enable is not necessary, it may be tied off or removed from the code.
 
 module data_memory#(
-  parameter MEMORY_WIDTH = 32,              // Specify RAM data width
+  parameter MEMORY_WIDTH = 8,              // Specify RAM data width
   parameter MEMORY_DEPTH = 128,              // Specify RAM depth (number of entries)
   parameter NB_ADDR = 7,
+  parameter NB_DATA = 32,
   parameter RAM_PERFORMANCE = "LOW_LATENCY",// Select "HIGH_PERFORMANCE" or "LOW_LATENCY" 
   parameter INIT_FILE = ""       // Specify name/location of RAM initialization file if using one (leave blank if not)
 ) 
 (
   input  i_clock,                            // Clock
   input  i_mem_write_flag,                   // Write enable
-  input  i_mem_read_flag,                    // Read Enable, for additional power savings, disable when not in use
-  input  rstb,                               // Output reset (does not affect memory contents)
-  input  regceb,                             // Output register enable
+  input  i_mem_read_flag,                    // Output register enable
+  input  i_word_en,
+  input  i_halfword_en,
+  input  i_byte_en,
   input  [NB_ADDR-1:0]      i_address,       // Write and read address bus, width determined from RAM_DEPTH
-  input  [MEMORY_WIDTH-1:0] i_write_data,    // RAM input data
-  output [MEMORY_WIDTH-1:0] o_read_data           // RAM output data
+  input  [NB_DATA-1:0] i_write_data,    // RAM input data
+  output [NB_DATA-1:0] o_read_data      // RAM output data
 );
 
   reg [MEMORY_WIDTH-1:0] BRAM [MEMORY_DEPTH-1:0];
-  reg [MEMORY_WIDTH-1:0] ram_data = {MEMORY_WIDTH{1'b0}};
+  reg [NB_DATA-1:0] ram_data = {NB_DATA{1'b0}};
 
 
   always @(posedge i_clock) begin
     if (i_mem_write_flag)
-      BRAM[i_address] <= i_write_data;
+      case ({i_word_en, i_halfword_en, i_byte_en})
+        3'b001:  BRAM[i_address][0]   <= i_write_data[7:0];
+        3'b010:  BRAM[i_address][1:0] <= i_write_data[15:0];
+        3'b100:  BRAM[i_address][3:0] <= i_write_data[31:0];
+        default: BRAM[i_address]      <= i_write_data;
+      endcase
     if (i_mem_read_flag)
-      ram_data <= BRAM[i_address];
+      case ({i_word_en, i_halfword_en, i_byte_en})
+        3'b001:  ram_data <= BRAM[i_address][0];
+        3'b010:  ram_data <= BRAM[i_address][1:0];
+        3'b100:  ram_data <= BRAM[i_address][3:0];
+        default: ram_data <= BRAM[i_address];
+      endcase
   end
 
   //  The following code generates HIGH_PERFORMANCE (use output register) or LOW_LATENCY (no output register)
@@ -40,20 +52,6 @@ module data_memory#(
 
       // The following is a 1 clock cycle read latency at the cost of a longer clock-to-out timing
        assign o_read_data = ram_data;
-
-    end else begin: output_register
-
-      // The following is a 2 clock cycle read latency with improve clock-to-out timing
-
-      reg [MEMORY_WIDTH-1:0] doutb_reg = {MEMORY_WIDTH{1'b0}};
-
-      always @(posedge i_clock)
-        if (rstb)
-          doutb_reg <= {MEMORY_WIDTH{1'b0}};
-        else if (regceb)
-          doutb_reg <= ram_data;
-
-      assign o_read_data = doutb_reg;
 
     end
   endgenerate
