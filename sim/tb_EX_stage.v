ea47a63fa8eb394d1e8ee42ba25ee16689dd532c
JimenaVega <jimevega28@gmail.com>
2023-03-17 09:58:06 -0300

    EX testbench con solo instrucciones tipo R

--- sim/tb_EX_stage.v
+++ sim/tb_EX_stage.v
@@ -90,85 +90,216 @@ module EX_stage_tb;
   endgenerate
 
   initial begin
+
+    $display("--------------------------------------");
+    $display("Starting Tests for R inst");
+    
+    // valores hardcodeados
     i_clock = 1'b0;
     op_counter = 0;
     tests_counter = 0;
-    // i_EX_reg_write = 1'b0;
-    // i_EX_mem_to_reg = 1'b0;
-    // i_EX_mem_read = 1'b0;
-    // i_EX_mem_write = 1'b0;
-    // i_EX_branch = 1'b0;
+    i_EX_pc = 32'b0; // PC
+    i_EX_alu_src = 1'b1; // Se prueban operaciones tipo R, esta flag siempre a a ser 1
+    i_EX_reg_dst = 1'b1; // La salida del MUX siempre va a ser rd
 
-    $display("--------------------------------------");
-    $display("Starting Tests");
-    #40
-    // add $s0,$s1,$s2 -> 000000 10001 10010 10000 00000 100000
-    i_EX_alu_src = 1'b0; // rt
-    i_EX_reg_dst = 1'b1; // rd
-    i_EX_alu_op = 6'b000000; // R-type
-    i_EX_pc = 32'h0;
-    i_EX_data_a = 32'he;
-    i_EX_data_b = 32'hf;
-    i_EX_immediate = 32'h20; // Se usa solo funct
-    i_EX_rt = 5'b10010; // $s2 NO deberia usarse.
-    i_EX_rd = 5'b10000; // $s0
+    // flags no usadas en este stage
+    i_EX_reg_write = 1'b0;
+    i_EX_mem_to_reg = 1'b0;
+    i_EX_mem_read = 1'b0;
+    i_EX_mem_write = 1'b0;
+    i_EX_branch = 1'b0;
 
     for(op_counter = 0; op_counter <= N_OP; op_counter = op_counter+1)begin
            
       for(tests_counter = 0; tests_counter <= N_tests; tests_counter = tests_counter+1)begin
-          #100
+          #40
           i_EX_data_a = $urandom;
-          // switches = i_EX_data_a;
-          // buttons = 3'd1;
-
-          #100
+         
+          #40
           i_EX_data_b = $urandom;
-          // switches = i_EX_data_b;
-          // buttons = 3'd2;
-
-          #100
-          // TODO: completar
-          i_EX_alu_op = instruction[op_counter][31:-6];
-          buttons = 3'd4;
+       
+          #40
+          i_EX_immediate = {16'b0, instruction[op_counter][16:0]}; // immediate incluido funct_code
+          i_EX_rt = instruction[op_counter][20:16];
+          i_EX_rd = instruction[op_counter][15:11];
           
-          #100
-          case(instruction[op_counter])
-              32: if(i_EX_data_a + i_EX_data_b !== o_EX_alu_result) begin
-                      $display("%b + %b = %b", i_EX_data_a, i_EX_data_b, o_EX_alu_result);
-                      $display("Error en la suma");
-                  end
-              34: if(i_EX_data_a - i_EX_data_b !== o_EX_alu_result) begin
-                      $display("%b - %b = %b", i_EX_data_a, i_EX_data_b, o_EX_alu_result);
-                      $display("Error en la resta");
-                  end
-              36: if((i_EX_data_a & i_EX_data_b) !== o_EX_alu_result) begin
-                      $display("%b & %b = %b", i_EX_data_a, i_EX_data_b, o_EX_alu_result);
-                      $display("Error en la and");
-                  end
-              37: if((i_EX_data_a | i_EX_data_b) !== o_EX_alu_result) begin
-                      $display("%b | %b = %b)", i_EX_data_a, i_EX_data_b, o_EX_alu_result);
-                      $display("Error en la or");
-                  end
-              38: if((i_EX_data_a ^ i_EX_data_b) !== o_EX_alu_result) begin
-                      $display("%b ^ %b = %b", i_EX_data_a, i_EX_data_b, o_EX_alu_result);
-                      $display("Error en la xor");
-                  end
-              3:  begin
-                      i_EX_data_b[NB_INPUTS-1] = 1'b0; //Quitamos el signo al dato B que nos indica cuantas veces shifteamos el dato A.
+          #40
+          i_EX_alu_op = instruction[op_counter][31:26];
+          
+          #40
+          if(!instruction[op_counter][31:26]) begin //R-type
+            
+            case(instruction[op_counter][6:0]) // funct
+              6'b100000: begin // add
+                if(i_EX_data_a + i_EX_data_b !== o_EX_alu_result) begin
+                  $display("%b + %b = %b", i_EX_data_a, i_EX_data_b, o_EX_alu_result);
+                  $display("Error en la suma");
+                end
+              end
+              6'b100001: begin // addu
+                if(i_EX_data_a + i_EX_data_b !== o_EX_alu_result) begin
+                  $display("%b + %b = %b", i_EX_data_a, i_EX_data_b, o_EX_alu_result);
+                  $display("Error en la suma");
+                end
+              end
+              6'b100010: begin // sub
+                if(i_EX_data_a - i_EX_data_b !== o_EX_alu_result) begin
+                  $display("%b - %b = %b", i_EX_data_a, i_EX_data_b, o_EX_alu_result);
+                  $display("Error en la resta");
+                end
+              end
+              6'b100011: begin  // subu
+                if(i_EX_data_a - i_EX_data_b !== o_EX_alu_result) begin
+                  $display("%b - %b = %b", i_EX_data_a, i_EX_data_b, o_EX_alu_result);
+                  $display("Error en la resta");
+                end
+              end
+              6'b100100: begin // and
+                if((i_EX_data_a & i_EX_data_b) !== o_EX_alu_result) begin
+                  $display("%b & %b = %b", i_EX_data_a, i_EX_data_b, o_EX_alu_result);
+                  $display("Error en la and");
+                end
+              end
+              6'b100111: begin //nor
+                if((~(i_EX_data_a | i_EX_data_b)) != o_EX_alu_result) begin
+                  $display("~(%b | %b) = %b", i_EX_data_a, i_EX_data_b, o_EX_alu_result);
+                  $display("Error en la nor");
+                end
+              end
+              6'b100101: begin // or
+                if((i_EX_data_a | i_EX_data_b) !== o_EX_alu_result) begin
+                  $display("%b | %b = %b)", i_EX_data_a, i_EX_data_b, o_EX_alu_result);
+                  $display("Error en la or");
+                end
+              end
+              6'b100110: begin // xor
+                if((i_EX_data_a ^ i_EX_data_b) !== o_EX_alu_result) begin
+                  $display("%b ^ %b = %b", i_EX_data_a, i_EX_data_b, o_EX_alu_result);
+                  $display("Error en la xor");
+                end
+              end
+              // TODO: Agregar shifts
+            endcase
+          end
+          if(instruction[op_co])
+          case(instruction[op_counter][31:26])
+              6'b100000: begin
+                if(i_EX_data_a + i_EX_immediate !== o_EX_alu_result) begin // lb
+                  $display("%b + %b = %b", i_EX_data_a, i_EX_data_b, o_EX_alu_result);
+                  $display("Error en lb");
+                end
+              end
+              6'b100001: begin // lh
+                if(i_EX_data_a + i_EX_immediate !== o_EX_alu_result) begin // lb
+                  $display("%b + %b = %b", i_EX_data_a, i_EX_data_b, o_EX_alu_result);
+                  $display("Error en lh");
+                end
+              end
+              6'b100010: begin // lhu
+                if(i_EX_data_a + i_EX_immediate !== o_EX_alu_result) begin // lb
+                  $display("%b + %b = %b", i_EX_data_a, i_EX_data_b, o_EX_alu_result);
+                  $display("Error en lhu");
+                end
+              end
+              6'b100011: begin // lw
+                if(i_EX_data_a + i_EX_immediate !== o_EX_alu_result) begin // lb
+                  $display("%b + %b = %b", i_EX_data_a, i_EX_data_b, o_EX_alu_result);
+                  $display("Error en lw");
+                end
+              end
+              6'b100100: begin // lwu
+                if(i_EX_data_a + i_EX_immediate !== o_EX_alu_result) begin // lb
+                  $display("%b + %b = %b", i_EX_data_a, i_EX_data_b, o_EX_alu_result);
+                  $display("Error en lwu");
+                end
+              end
+              6'b100101: begin // lbu
+                if(i_EX_data_a + i_EX_immediate !== o_EX_alu_result) begin // lb
+                  $display("%b + %b = %b", i_EX_data_a, i_EX_data_b, o_EX_alu_result);
+                  $display("Error en lbu");
+                end
+              end
+              6'b101000: begin // sb
+                if(i_EX_data_a + i_EX_immediate !== o_EX_alu_result) begin // lb
+                  $display("%b + %b = %b", i_EX_data_a, i_EX_data_b, o_EX_alu_result);
+                  $display("Error en sb");
+                end
+              end
+              6'b101001: begin // sh
+                if(i_EX_data_a + i_EX_immediate !== o_EX_alu_result) begin // lb
+                  $display("%b + %b = %b", i_EX_data_a, i_EX_data_b, o_EX_alu_result);
+                  $display("Error en sw");
+                end
+              end
+              6'b101011: begin // sw
+                if(i_EX_data_a + i_EX_immediate !== o_EX_alu_result) begin // lb
+                  $display("%b + %b = %b", i_EX_data_a, i_EX_data_b, o_EX_alu_result);
+                  $display("Error en sw");
+                end
+              end
+              6'b001000: begin // addi
+                if(i_EX_data_a + i_EX_immediate !== o_EX_alu_result) begin // lb
+                  $display("%b + %b = %b", i_EX_data_a, i_EX_data_b, o_EX_alu_result);
+                  $display("Error en addi");
+                end
+              end
+              6'b001101: begin // andi
+                if((i_EX_data_a & i_EX_immediate) !== o_EX_alu_result) begin
+                  $display("%b & %b = %b", i_EX_data_a, i_EX_data_b, o_EX_alu_result);
+                  $display("Error en andi");
+                end
+              end
+              6'b001101: begin // ori
+                if((i_EX_data_a | i_EX_immediate) !== o_EX_alu_result) begin
+                  $display("%b | %b = %b)", i_EX_data_a, i_EX_data_b, o_EX_alu_result);
+                  $display("Error en la or");
+                end
+              end
+              6'b001111: begin // lui
+                if((i_EX_data_a << 16) !== o_EX_alu_result) begin
+                  $display("%b << 16 = %b", i_EX_data_a, o_EX_alu_result);
+                  $display("Error en lui");
+                end
+              end
+              6'b001010: begin // slti
+                if(i_EX_data_a < i_EX_immediate != o_EX_alu_result) begin
+                  $display("%b < %b = %b", i_EX_data_a, i_EX_data_b, o_EX_alu_result);
+                  $display("Error en slti");
+                end
+              end
+              
+              // 34: if(i_EX_data_a - i_EX_immediate !== o_EX_alu_result) begin
+              //         $display("%b - %b = %b", i_EX_data_a, i_EX_data_b, o_EX_alu_result);
+              //         $display("Error en la resta");
+              //     end
+              // 36: if((i_EX_data_a & i_EX_immediate) !== o_EX_alu_result) begin
+              //         $display("%b & %b = %b", i_EX_data_a, i_EX_data_b, o_EX_alu_result);
+              //         $display("Error en la and");
+              //     end
+              // 37: if((i_EX_data_a | i_EX_immediate) !== o_EX_alu_result) begin
+              //         $display("%b | %b = %b)", i_EX_data_a, i_EX_data_b, o_EX_alu_result);
+              //         $display("Error en la or");
+              //     end
+              // 38: if((i_EX_data_a ^ i_EX_immediate) !== o_EX_alu_result) begin
+              //         $display("%b ^ %b = %b", i_EX_data_a, i_EX_data_b, o_EX_alu_result);
+              //         $display("Error en la xor");
+              //     end
+              // 3:  begin
+              //         i_EX_data_b[NB_INPUTS-1] = 1'b0; //Quitamos el signo al dato B que nos indica cuantas veces shifteamos el dato A.
                       
-                      if((i_EX_data_a >>> i_EX_data_b) !== o_EX_alu_result) begin
-                          $display("%b >>> %b = %b but expected: %b", i_EX_data_a, i_EX_data_b, o_EX_alu_result, (i_EX_data_a >>> i_EX_data_b));
-                          $display("Error en la sra");
-                      end
-                  end
-              2:  if((i_EX_data_a >> i_EX_data_b) !== o_EX_alu_result) begin
-                      $display("%b >> %b = %b", i_EX_data_a, i_EX_data_b, o_EX_alu_result);
-                      $display("Error en la srl");
-                  end
-              39: if((~(i_EX_data_a | i_EX_data_b)) != o_EX_alu_result) begin
-                      $display("~(%b | %b) = %b", i_EX_data_a, i_EX_data_b, o_EX_alu_result);
-                      $display("Error en la nor");
-                  end
+              //         if((i_EX_data_a >>> i_EX_data_b) !== o_EX_alu_result) begin
+              //             $display("%b >>> %b = %b but expected: %b", i_EX_data_a, i_EX_data_b, o_EX_alu_result, (i_EX_data_a >>> i_EX_data_b));
+              //             $display("Error en la sra");
+              //         end
+              //     end
+              // 2:  if((i_EX_data_a >> i_EX_data_b) !== o_EX_alu_result) begin
+              //         $display("%b >> %b = %b", i_EX_data_a, i_EX_data_b, o_EX_alu_result);
+              //         $display("Error en la srl");
+              //     end
+              // 39: if((~(i_EX_data_a | i_EX_data_b)) != o_EX_alu_result) begin
+              //         $display("~(%b | %b) = %b", i_EX_data_a, i_EX_data_b, o_EX_alu_result);
+              //         $display("Error en la nor");
+              //     end
           endcase
       end
 
