9f588fb013067c4587d1cb5eb46a40f3021c34fd
JimenaVega <jimevega28@gmail.com>
2023-03-31 19:41:20 -0300

    Mofidicado IF stage para J,JAL,JR y JALR

--- sim/tb_IF_stage.v
+++ sim/tb_IF_stage.v
@@ -6,49 +6,68 @@ module tb_IF_stage;
   localparam  NB_INSTRUCTION = 32;
 
   // Ports
-  reg i_clock = 0;
-  reg i_IF_branch = 0;
-  reg i_IF_jump = 0;
-  reg i_IF_pc_enable = 0;
-  reg i_IF_pc_reset = 0;
-  reg i_IF_read_enable = 0;
+  reg i_clock;
+  reg i_IF_branch;
+  reg i_IF_j_jal;
+  reg i_IF_jr_jalr;
+  reg i_IF_pc_enable;
+  reg i_IF_pc_reset;
+  reg i_IF_read_enable;
   reg [NB_PC-1:0] i_IF_branch_addr;
   reg [NB_PC-1:0] i_IF_jump_address;
+  reg [NB_PC-1:0] i_IF_r31_data, 
   wire [NB_PC-1:0] o_IF_adder_result;
   wire [NB_INSTRUCTION-1:0] o_IF_new_instruction;
 
   IF_stage 
   #(
     .NB_PC_CONSTANT(NB_PC_CONSTANT ),
-    .NB_PC(NB_PC ),
-    .NB_INSTRUCTION (
-        NB_INSTRUCTION )
+    .NB_PC(NB_PC),
+    .NB_INSTRUCTION(NB_INSTRUCTION )
   )
   IF_stage_1 (
     .i_clock (i_clock ),
     .i_IF_branch (i_IF_branch ),
-    .i_IF_jump (i_IF_jump ),
-    .i_IF_pc_enable (i_IF_pc_enable ),
+    .i_IF_j_jal (i_IF_j_jal),
+    .i_IF_jr_jalr(i_IF_jr_jalr),
+    .i_IF_pc_enable (i_IF_pc_enable),
     .i_IF_pc_reset (i_IF_pc_reset ),
     .i_IF_read_enable (i_IF_read_enable ),
     .i_IF_branch_addr (i_IF_branch_addr ),
     .i_IF_jump_address (i_IF_jump_address ),
+    .i_IF_r31_data(i_IF_r31_data),
     .o_IF_adder_result (o_IF_adder_result ),
     .o_IF_new_instruction  ( o_IF_new_instruction)
   );
 
   initial begin
     begin
+      i_clock = 0;
       i_IF_pc_reset = 1'b1;
+      i_IF_branch = 1'b0;
+      i_IF_j_jal = 1'b0;
+      i_IF_jr_jalr = 1'b0;
+      i_IF_branch_addr = 32'h4;
+      i_IF_jump_address = 32'h3;
+      i_IF_r31_data = 32'h2;
       
       #40
-      
       i_IF_read_enable = 1'b1;
-
+      // PC + 1
       #40
-      
       i_IF_pc_reset = 1'b0;
       i_IF_pc_enable = 1'b1;
+      // branch address
+      #40
+      i_IF_branch = 1'b1;
+      
+      // j/jal address
+      #40
+      i_IF_j_jal = 1'b1;
+
+      // jr/jalrl address
+      #40
+      i_IF_jr_jalr = 1'b1;
 
       #1000
 
--- src/IF_stage.v
+++ src/IF_stage.v
@@ -7,15 +7,17 @@ module IF_stage#(
     )
     (
         input                       i_clock,
-        input                       i_IF_branch, // 1 -> Hay branch, 0 -> No hay branch
-        input                       i_IF_jump,   // 1 -> Hay jump, 0 -> No hay jump
+        input                       i_IF_branch,  // 1 -> Hay branch, 0 -> No hay branch
+        input                       i_IF_j_jal,   // 1 -> Hay jump, 0 -> No hay jump
+        input                       i_IF_jr_jalr, // 1 -> Hay jump, 0 -> No hay jump
         input                       i_IF_pc_enable,
         input                       i_IF_pc_reset,
         input                       i_IF_read_enable,
         input  [NB_PC-1:0]          i_IF_branch_addr,
-        input  [NB_PC-1:0]          i_IF_jump_address,
+        input  [NB_PC-1:0]          i_IF_jump_address, // J y JAL
+        input  [NB_PC-1:0]          i_IF_r31_data,     // JR y JALR
         
-        output [NB_PC-1:0]          o_IF_adder_result,
+        output [NB_PC-1:0]          o_IF_adder_result, // PC+1
         output [NB_INSTRUCTION-1:0] o_IF_new_instruction
     );
     
@@ -31,7 +33,7 @@ module IF_stage#(
     program_counter program_counter_1(.i_enable(i_IF_pc_enable),
                                       .i_clock(i_clock),
                                       .i_reset(i_IF_pc_reset),
-                                      .i_mux_pc(mux2_2_output),
+                                      .i_mux_pc(mux2_3_output),
                                       .o_pc_mem(new_pc_value));
     
     adder adder_1(.i_a(new_pc_value),
@@ -39,14 +41,19 @@ module IF_stage#(
                   .o_result(adder_result));
     
     mux2 mux2_1(.i_select(i_IF_branch),
-                .i_a(adder_result),
-                .i_b(i_IF_branch_addr),
+                .i_a(adder_result),     // PC+1
+                .i_b(i_IF_branch_addr), // branch
                 .o_data(mux2_1_output));
                 
-    mux2 mux2_2(.i_select(i_IF_jump),
+    mux2 mux2_2(.i_select(i_IF_j_jal),
                 .i_a(mux2_1_output),
                 .i_b(i_IF_jump_address),
                 .o_data(mux2_2_output));
+
+    mux2 mux2_3(.i_select(i_IF_r31_data),
+                .i_a(mux2_2_output),
+                .i_b(i_IF_r31_data),
+                .o_data(mux2_3_output));            
     
     instruction_memory instruction_memory_1(.i_clock(i_clock),
                                             .i_read_enable(i_IF_read_enable),
