e6e9c997382f5c243a9584c3d0dc46e587aa732b
JimenaVega <jimevega28@gmail.com>
2023-05-29 20:16:08 -0300

    Agregado handler para cerrar puerto

--- GUI/test_uart.py
+++ GUI/test_uart.py
@@ -1,9 +1,28 @@
 import time
 import serial
+import signal
 
 # Port busy: sudo lsof /dev/ttyUSB0
 # sudo kill -9
 #ser = serial.serial_for_url('loop://', timeout=1)
+
+def handler(signum, frame):
+    msg = "Ctrl-c was pressed. "
+    print(msg)
+    ser.reset_input_buffer()
+    ser.reset_output_buffer()    
+    ser.close() 
+    exit()
+ 
+def byte_to_bistring(intnum):
+    # intnum = 15
+    converted = int.from_bytes(intnum, 'big')
+    bistring = bin(converted)[2:]  # Obtiene la representación binaria y omite el prefijo "0b"
+    bistring = bistring.zfill(8)  # Rellena con ceros a la izquierda hasta tener 8 bits
+    
+    print("R: ", bistring)  # Imprime el string binario resultante
+
+    return bistring
 
 ser = serial.Serial(
             port     = '/dev/ttyUSB0',	#Configurar con el puerto
@@ -12,6 +31,8 @@ ser = serial.Serial(
             stopbits = serial.STOPBITS_ONE,
             bytesize = serial.EIGHTBITS
         )
+
+signal.signal(signal.SIGINT, handler)
 
 def main():
 
