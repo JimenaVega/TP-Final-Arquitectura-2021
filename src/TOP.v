1deb97d4f747df5fb5af4a89b151997216a7c0a0
AlejoSev <alejoseverini@gmail.com>
2023-06-20 09:26:59 -0300

    Warning fix

--- src/TOP.v
+++ src/TOP.v
@@ -17,8 +17,6 @@ module TOP#(
         output                o_uart_du_tx,
         output                o_hlt,
         output [NB_STATE-1:0] o_state,
-        output                o_clk, // borrar
-        output                o_led_rx_done, //borrar
         output                o_pc_value
     );
 
@@ -148,8 +146,6 @@ module TOP#(
     assign o_state      = state;
     assign o_uart_du_tx = uart_du_tx;
     assign o_hlt        = halt;
-    assign o_clk        = i_clock; // borrar
-    assign o_led_rx_done = uart_du_rx_done;
     assign o_pc_value = pc[0];
     
 endmodule
